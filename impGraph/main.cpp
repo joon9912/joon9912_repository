71139601f0c76e6996db16972b21b5113039b2b4
joon9912@mail.hongik.ac.kr <joon9912@mail.hongik.ac.kr>
2023-07-04 14:59:45 +0900

    implement weighted directed graph

--- impGraph/main.cpp
+++ impGraph/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 #include <vector>
 using namespace std;
 
@@ -6,6 +7,7 @@ using namespace std;
 struct Edge {
     int src;
     int dst;
+    int weight;
 };
 
 class Graph {
@@ -38,18 +40,52 @@ void printGraph(Graph const &graph, int n) {
     }
 }
 
+typedef pair<int, int> Pair;
+
+class WeightGraph {
+public:
+    vector<vector<Pair>> adjList;
+
+    WeightGraph(vector<Edge> const &edges, int n) {
+        adjList.resize(n);
+
+        for (auto &edge : edges) {
+            int src = edge.src;
+            int dst = edge.dst;
+            int weight = edge.weight;
+
+            adjList[src].push_back(make_pair(dst, weight));
+
+            // undirected graph
+            // adjList[dst].push_back(make_pair(src, weight));
+        }
+    }
+};
+
+void printWeightGraph(WeightGraph const &graph, int n) {
+    for (int i = 0; i < n; ++i) {
+        for (Pair v : graph.adjList[i]) {
+            cout << "(" << i << ", " << v.first << ", " << v.second << ") ";
+        }
+        cout << "\n";
+    }
+}
 // implement Graph by using STL
 int main() {
     int n = 6;
     vector<Edge> edges = {
-            {0, 1},{1, 2},
-            {2, 0},{2, 1},
-            {3, 2},{4, 5},
-            {5, 4},
+            {0, 1, 6},
+            {1, 2, 7},
+            {2, 0, 5},
+            {2,1,4},
+            {3,2,10},
+            {5,4,1},
+            {4,5,3},
     };
-    Graph graph(edges, n);
 
-    printGraph(graph, n);
+    WeightGraph graph(edges, n);
+
+    printWeightGraph(graph, n);
 
     return 0;
 }
