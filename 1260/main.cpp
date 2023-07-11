ada84655f60900033a6702f8df11d302e80b43a8
joon9912@mail.hongik.ac.kr <joon9912@mail.hongik.ac.kr>
2023-07-11 16:02:29 +0900

    solve 1260 (dfs and bfs) using queue and 2-dimension array

---
+++ 1260/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N, M, V, x, y;
+int arr[1001][1001];
+int visited[1001];
+queue<int> q;
+
+void dfs(int x) {
+    cout << x << " ";
+    visited[x] = 1;
+    for (int i = 1; i <= N; ++i) {
+        if (arr[x][i] && !visited[i])
+            dfs(i);
+    }
+}
+
+void bfs(int x) {
+    q.push(x);
+    visited[x] = 1;
+    cout << x << " ";
+    while (!q.empty()) {
+        int top = q.front();
+        q.pop();
+
+        for (int i = 1; i <= N; ++i) {
+            if (arr[top][i] && !visited[i]) {
+                cout << i << " ";
+                q.push(i);
+                visited[i] = visited[top] + 1; // visited #
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N >> M >> V;
+
+    for (int i = 0; i < M; ++i) {
+        cin >> x >> y;
+
+        arr[x][y] = 1;
+        arr[y][x] = 1;
+    }
+
+    dfs(V); cout << "\n";
+
+    for (int i = 1; i <= N; ++i)
+        visited[i] = 0;
+
+    bfs(V); cout << "\n";
+
+    return 0;
+}
