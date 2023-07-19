2408df6049f1c8c09413b39f678122ac6f7838a2
joon9912@mail.hongik.ac.kr <joon9912@mail.hongik.ac.kr>
2023-07-19 11:24:25 +0900

    solve 21758 using greedy need more thinking how to access greedy alg

--- 21758/main.cpp
+++ 21758/main.cpp
@@ -1,85 +1,43 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int N, x, maximum;
-vector<int> v;
+#define SIZE 100001
+
+int N, s, maximum;
+int honey[SIZE];
+int sum[SIZE];
 
 int main() {
     cin >> N;
 
-    for (int i = 0; i < N; ++i) {
-        cin >> x;
-        v.push_back(x);
+    for (int i = 1; i <= N; ++i) {
+        cin >> honey[i];
+        sum[i] = sum[i - 1] + honey[i];
     }
 
     int honeycomb;
-    for (int i = 0; i < N; ++i) {
-        honeycomb = i;
-
-        int h1 = 0 , h2 = 0;
-        while (h1 == honeycomb) {
-            h1++;
-        }
-        while (h2 == honeycomb || h1 == h2) {
-            h2++;
-        }
-        // cout << h1 << " " << h2 << "\n";
-
-        while (1) {
-            int s1 = 0, s2 = 0;
-            if (h1 < honeycomb && h2 < honeycomb) {
-                for (int j = h1 + 1; j <= honeycomb; ++j) {
-                    if (j != h2)
-                        s1 += v[j];
-                }
-                for (int j = h2 + 1; j <= honeycomb; ++j){
-                    s2 += v[j];
-                }
-            }
-            if (h1 < honeycomb && h2 > honeycomb) {
-                for (int j = h1 + 1; j <= honeycomb; ++j) {
-                    s1 += v[j];
-                }
-                for (int j = honeycomb; j < h2; ++j) {
-                    s2 += v[j];
-                }
-            }
-            if (h1 > honeycomb && h2 > honeycomb) {
-                for (int j = honeycomb; j < h1; ++j) {
-                    s1 += v[j];
-                }
-                for (int j = honeycomb; j < h2; ++j) {
-                    if (j != h1)
-                        s2 += v[j];
-                }
-            }
-            // cout << h1 << " " << h2 << " " << s1 + s2 << "\n" ;
-            maximum = max(maximum, s1 + s2);
-
-            do {
-                h2++;
-            } while (h2 == honeycomb);
-
-            if (h2 == N) {
-                do {
-                    h1++;
-                } while (h1 == honeycomb);
-                h2 = h1 + 1;
-                while (h2 == honeycomb) {
-                    h2++;
-                }
-            }
-            if (h2 == N)
-                break;
-        }
+    // h1 - honeycomb - h2
+    for (int i = 2; i < N; ++i) {
+        s += (sum[i] - honey[1]) + (sum[N] - honey[N] - sum[i - 1]);
+        maximum = max(maximum, s);
+        s = 0;
     }
 
-    cout << maximum << endl;
+    // h1 - h2 - honeycomb
+    for (int i = 2; i < N; ++i) {
+        s += (sum[N] - honey[1] - honey[i]) + (sum[N] - sum[i]);
+        maximum = max(maximum, s);
+        s = 0;
+    }
+
+    // honeycomb - h1 - h2
+    for (int i = 2; i < N; ++i) {
+        s += (sum[N] - honey[N] - honey[i]) + (sum[i] - honey[i]);
+        maximum = max(maximum, s);
+        s = 0;
+    }
+
+    cout << maximum << "\n";
 
     return 0;
 }
-
-/*
-
- */
