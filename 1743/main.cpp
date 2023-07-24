f6c10d38786385eca93dce1b044f70473210be4e
joon9912@mail.hongik.ac.kr <joon9912@mail.hongik.ac.kr>
2023-07-24 16:26:51 +0900

    solve 1743 using bfs in this case

--- 1743/main.cpp
+++ 1743/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <queue>
+#include <utility>
+
 using namespace std;
 
 int N, M, K;
 int arr[101][101];
 int visited[101][101];
+int dx[]{-1,0,1,0};
+int dy[]{0,1,0,-1};
+
 int ans, sum;
+queue<pair<int, int>> q;
 
 void dfs(int x, int y) {
     if (arr[x][y] && !visited[x][y]
@@ -14,10 +21,34 @@ void dfs(int x, int y) {
         visited[x][y] = 1;
         sum++;
 
-        dfs(x + 1, y);
-        dfs(x , y + 1);
-        dfs(x - 1, y);
-        dfs(x, y - 1);
+        for (int i = 0; i < 4; ++i) {
+            dfs(x + dx[i], y + dy[i]);
+        }
+    }
+    ans = max(ans, sum);
+}
+
+void bfs(int m, int n) {
+    // cout << m << " " << n << endl;
+    q.push(make_pair(m, n));
+    visited[m][n] = 1;
+    sum++;
+    while (!q.empty()) {
+        auto x = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; ++i) {
+            int nx = x.first + dx[i];
+            int ny = x.second + dy[i];
+
+            if (arr[nx][ny] && !visited[nx][ny]
+                && nx > 0 && nx <= N && ny > 0 && ny <= M)
+            {
+                visited[nx][ny] = 1;
+                q.push(make_pair(nx, ny));
+                sum++;
+            }
+        }
     }
     ans = max(ans, sum);
 }
@@ -27,6 +58,19 @@ void best() {
         for (int j = 1; j <= M; ++j) {
             dfs(i, j);
             sum = 0;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+void best2() {
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= M; ++j) {
+            if (arr[i][j] && !visited[i][j]) {
+                bfs(i, j);
+                sum = 0;
+            }
         }
     }
 
@@ -43,7 +87,8 @@ int main() {
         arr[x][y] = 1;
     }
 
-    best();
+    // best();
+    best2();
 
     return 0;
 }
